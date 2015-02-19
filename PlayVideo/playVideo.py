1ffe5e2c670e4e21a3764b20091f39ea2530de83
Christian <christian.liin.hansen@gmail.com>
2015-02-20 00:03:02 +0100

    Now with delay before taking image, in order to let autofocus settle

--- PlayVideo/playVideo.py
+++ PlayVideo/playVideo.py
@@ -7,21 +7,46 @@ Created on Sat Sep 13 16:20:45 2014
 
 import numpy as np
 import cv2
+import time
 
-cap = cv2.VideoCapture('indoor4.mp4')
-
-while(cap.isOpened()):
+def takeImageFromVideo(cap):
     ret, frame = cap.read()
+    cv2.imshow("Image taken", frame)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def main():
 
-    cv2.imshow('frame',gray)
+    # cap = cv2.VideoCapture('indoor4.mp4')  # In order to play a recorded video
+    cap = cv2.VideoCapture(2)  # In order to play the video streamed from a USB webcamera on video2
 
-    # Add some exqtra delay..
-    cv2.waitKey(10)
-    
-    # if we push "q" the program ececutes
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cv2.destroyAllWindows()
-cap.release()
+    # Set the camera in 1080p resolution. So the height is 1080, p = progressive scan = not interlaced. = "Full HD" = 1920 x 1080.
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1920) #the parameter is limited to 720 pixels
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 1080) # At 1024 the parameter is limited to 720 pixels
+
+    print "Width is:", cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
+    print "Height is:", cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
+
+    startTime = time.clock()
+    waitTime = 3
+    flag = False
+
+    while cap.isOpened():
+        k = cv2.waitKey(30) & 0xff
+        ret, frame = cap.read()
+        cv2.imshow('Streaming video', frame)
+
+        currentTime = time.clock()
+        # print "The difference in time is", currentTime - startTime
+        if currentTime - startTime > waitTime and flag is False:
+            takeImageFromVideo(cap)
+            flag = True
+
+        # if we push "q" the program ececutes
+        if k == 27:
+            print("User closed the program...")
+            break
+
+    cv2.destroyAllWindows()
+    cap.release()
+
+if __name__ == '__main__':
+    main()
