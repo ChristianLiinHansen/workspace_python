c67d61cbd0421b9bd098456a742fb2c26a9f3589
Christian <christian.liin.hansen@gmail.com>
2015-02-20 01:36:06 +0100

    PlayVideo and MasterThesis camera settings adjusted

--- PlayVideo/playVideo.py
+++ PlayVideo/playVideo.py
@@ -9,9 +9,18 @@ import numpy as np
 import cv2
 import time
 
-def takeImageFromVideo(cap):
+def takeImageFromVideo(cap, nameOfWindow):
     ret, frame = cap.read()
-    cv2.imshow("Image taken", frame)
+    offset_x = 400
+    offset_y = 200
+    imgCrop = cropImage(frame, offset_x, offset_y)
+    cv2.imshow(nameOfWindow, imgCrop)
+    cv2.imwrite("/home/christian/workspace_python/MasterThesis/SeedDetection/writefiles/" + str(nameOfWindow) + ".jpg", imgCrop)
+
+def cropImage(img, offset_x, offset_y):
+    crop_img = img.copy()
+    # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
+    return crop_img[offset_y:img.shape[0]-offset_y, offset_x:img.shape[1]-offset_x]
 
 def main():
 
@@ -19,8 +28,9 @@ def main():
     cap = cv2.VideoCapture(2)  # In order to play the video streamed from a USB webcamera on video2
 
     # Set the camera in 1080p resolution. So the height is 1080, p = progressive scan = not interlaced. = "Full HD" = 1920 x 1080.
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1920) #the parameter is limited to 720 pixels
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 1080) # At 1024 the parameter is limited to 720 pixels
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 1080)
+
 
     print "Width is:", cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
     print "Height is:", cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
@@ -37,7 +47,7 @@ def main():
         currentTime = time.clock()
         # print "The difference in time is", currentTime - startTime
         if currentTime - startTime > waitTime and flag is False:
-            takeImageFromVideo(cap)
+            takeImageFromVideo(cap, "ImageFromVideo")
             flag = True
 
         # if we push "q" the program ececutes
